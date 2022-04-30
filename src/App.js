266e83ce2e47974d2efa5ad884a3d3f6e64078bc
False Start <caroline.leopold@gmail.com>
2022-04-30 13:29:50 -0400

    make sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Link, Outlet } from 'react-router-dom';
 import './App.css';
+import './global.css';
 import './utils.css';
 
 function App() {
--- src/routes/guide.jsx
+++ src/routes/guide.jsx
@@ -1,13 +1,19 @@
 import {Link, Outlet} from 'react-router-dom';
-
+import '../App.css';
+import '../global.css';
+import '../utils.css';
 
 export default function Guide() {
   return (
     <>
-      <nav>
-
-      </nav>
-      <h2>Grant Guide</h2>
+      <div className="flex">
+        <nav className="sidebar">
+          <Link to="/sbir">SBIR</Link> {" "}
+          <Link to="/beginners">Beginners Guide</Link> {" "}
+          <Link to="/templates">Templates</Link>{" "}
+        </nav>
+        <h2>Grant Guide</h2>
+      </div>
       <Outlet />
      
     </>
