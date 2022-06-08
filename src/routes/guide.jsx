76a1cb0a5a505e22c1f8cb4d4aca75b8033abd21
False Start <caroline.leopold@gmail.com>
2022-06-08 15:37:30 -0400

    moved from mdx to md

--- src/routes/guide.jsx
+++ src/routes/guide.jsx
@@ -2,6 +2,7 @@ import {Link, Outlet} from 'react-router-dom';
 import '../App.css';
 import '../global.css';
 import '../utils.css';
+import '../content.md';
 
 export default function Guide() {
   return (
